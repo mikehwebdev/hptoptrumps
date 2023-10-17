7ed1d4df2cc674a173b2f160c21fff2ec53aa3e2
mikehwebdev <81847609+mikehwebdev@users.noreply.github.com>
2023-10-17 12:32:29 +0100

    Testing battle turn system

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
-import { diceRoll, render, splitDeck, initiateBattle } from "./utils"
+import { diceRoll} from "./utils"
+import { characters } from "./data"
+
 
 const welcomePageSection = document.querySelector('.welcome')
 const choosePlayersSection = document.querySelector('.choose-players')
@@ -19,7 +21,6 @@ const player1InputEl = document.querySelector('.player1-inputs')
 const player2InputEl = document.querySelector('.player2-inputs')
 
 const chooseDeck = document.querySelector('.deck-container')
-
 const gryffindorDeckEl = document.getElementById('gryffindorDeck')
 const slytherinDeckEl = document.getElementById('slytherinDeck')
 const ravenclawDeckEl = document.getElementById('ravenclawDeck')
@@ -32,12 +33,23 @@ const dice2El = document.querySelector('.dice2')
 
 const rollResultEl = document.querySelector('.roll-result')
 
-let player1turn = true
+const cardsInPlay = document.querySelector('.cards-in-play')
+const winnerDialog = document.querySelector('.winner-message')
+const winnerDialogText = document.querySelector('.winner-message-text')
+const nextRoundBtn = document.querySelector('.next-round')
 
-let gameType = ''
+let player1Turn = true
+let renderCycle = 1
 
 let diceScore1 = 0
 let diceScore2 = 0
+
+let gameType = ''
+
+let battleDeck = []
+
+let player1Deck = []
+let player2Deck = []
 
 // Event listeners for navigation 
 
@@ -74,16 +86,16 @@ playerCountEl.addEventListener('click', (e)=>{
   }
 })
 
-//setPlayerNames checks for name values entered and sets them in localStorage for later use. If no value is entered defaults are used instead
+  //setPlayerNames checks for name values entered and sets them in localStorage for later use. If no value is entered defaults are used instead
 
-function setPlayerNames(){
-  player1El.value ? localStorage.setItem('p1name', player1El.value) : localStorage.setItem('p1name', 'Player 1')
-  if (gameType === 'multi') {
-  player2El.value ? localStorage.setItem('p2name', player2El.value) : localStorage.setItem('p2name', 'Player 2')
-  } else {
-    localStorage.setItem('p2name', 'Computer')
+  function setPlayerNames(){
+    player1El.value ? localStorage.setItem('p1name', player1El.value) : localStorage.setItem('p1name', 'Player 1')
+    if (gameType === 'multi') {
+    player2El.value ? localStorage.setItem('p2name', player2El.value) : localStorage.setItem('p2name', 'Player 2')
+    } else {
+      localStorage.setItem('p2name', 'Computer')
+    }
   }
-}
 
 // deckManipulator loops through all cards in the deck selection section and grows/shrinks them as appropriate
 
@@ -155,17 +167,17 @@ diceRollBtn.addEventListener('click', ()=>{
     dice1El.innerHTML = `<img src="./images/${diceScore1}.png">`
     dice2El.innerHTML = `<img src="./images/${diceScore2}.png">`
     diceCount = diceCount +1
-  if (diceCount === 20) {
+  if (diceCount === 2) {
     clearInterval(diceInterval)
 
     if (diceScore1 > diceScore2) {
       rollResultEl.textContent = `${localStorage.getItem('p1name')} wins.`
-      player1turn = true
+      player1Turn = true
       diceRollBtn.style.display = 'none'
       dealCardsBtn.style.display = 'block'
     
     } else if (diceScore2 > diceScore1) {
-      player1turn = false
+      player1Turn = false
       rollResultEl.textContent = `${localStorage.getItem('p2name')} wins.`
       diceRollBtn.style.display = 'none'
       dealCardsBtn.style.display = 'block'
@@ -181,15 +193,103 @@ diceRollBtn.addEventListener('click', ()=>{
 })
 
 dealCardsBtn.addEventListener('click',()=>{
-
   splitDeck()
   rollDiceSection.classList.toggle('visible')
   battlefieldSection.classList.toggle('visible')
   initiateBattle()
 })
 
+function splitDeck(){
+  characters.forEach((character)=>{
+    character.id %2 == 0? player1Deck.push(character) : player2Deck.push(character);
+    // character.id === 0? player2Deck.push(character) : player1Deck.push(character);
+})
+}
+
+  function initiateBattle (){
+    battleDeck.unshift(player1Deck[0])
+    battleDeck.unshift(player2Deck[0])
+    player1Deck.shift()
+    player2Deck.shift()
+    render()
+  }
+
+function render(){
+  let player1ActiveCard = new TrumpCard(battleDeck[0])
+  let player2ActiveCard = new TrumpCard(battleDeck[1])
+  cardsInPlay.innerHTML = `${player1ActiveCard.getCardHtml()}` 
+  renderCycle = 2
+  cardsInPlay.innerHTML += `${player2ActiveCard.getCardHtml()}`
+  renderCycle = 1
+}
+
+cardsInPlay.addEventListener('click', (e)=>{
+    console.log(e)
+    if (battleDeck[0][e.target.parentElement.id] > battleDeck[1][e.target.parentElement.id]) {
+       console.log('player1 wins')
+       player1Turn = true
+       player1Deck.push(battleDeck[0],battleDeck[1])
+       displayWinner()
+
+  } else if (battleDeck[0][e.target.parentElement.id] < battleDeck[1][e.target.parentElement.id]){
+    console.log('player2 wins')
+    player1Turn = false
+    player2Deck.push(battleDeck[0],battleDeck[1])
+    displayWinner()
+  } else if (battleDeck[0][e.target.parentElement.id] === battleDeck[1][e.target.parentElement.id]){
+    console.log('draw')
+    player1Deck.push(battleDeck[0])
+    player2Deck.push(battleDeck[1])
+    displayWinner()
+  }
+  
+})
+
+function displayWinner(){
+  winnerDialog.style.display = 'grid'
+  winnerDialogText.innerHTML = 
+  player1Turn? `${localStorage.getItem('p1name')} wins!`: `${localStorage.getItem('p2name')} wins!` 
+}
+
+nextRoundBtn.addEventListener('click',()=>{
+  battleDeck = []
+  winnerDialog.style.display = 'none'
+  initiateBattle()
+})
 
 
+class TrumpCard {
+  constructor(data){
+    Object.assign(this,data) 
+  }
 
+  renderCalculator(){
+      if (renderCycle === 1 && player1Turn) {
+      return 'active'
+    } else if (renderCycle === 2 && !player1Turn) {
+      return 'active'
+    } else {
+      return ''
+    }
+  }
 
-
+  getCardHtml(){
+    const {name, img, magic, cunning, courage, wisdom, temper} = this
+    return ` <div class="card ${this.renderCalculator()}">
+    
+      <div class="card-inner">
+    <h3 class="card-name">${name}</h3>
+    <div class="card-image-container">
+      <img src=${img} class="card-image">
+    </div>
+    <ul class="card-attribute-container">
+      <li id="magic" class="magic ${player1Turn? 'Player1' : 'Player2'}"><span class="attrib-name">Magic</span><span class="arrow">&#10148;</span><span class="attrib-value">${magic}</span></li>
+      <li id="cunning" class="cunning ${player1Turn? 'Player1' : 'Player2'}"><span class="attrib-name">Cunning</span><span class="arrow">&#10148;</span><span class="attrib-value">${cunning}</span></li>
+      <li id="courage" class="courage ${player1Turn? 'Player1' : 'Player2'}"><span class="attrib-name">Courage</span><span class="arrow">&#10148;</span><span class="attrib-value">${courage}</span></li>
+      <li id="wisdom" class="wisdom ${player1Turn? 'Player1' : 'Player2'}"><span class="attrib-name">Wisdom</span><span class="arrow">&#10148;</span><span class="attrib-value">${wisdom}</span></li>
+      <li id="temper" class="temper ${player1Turn? 'Player1' : 'Player2'}"><span class="attrib-name">Temper</span><span class="arrow">&#10148;</span> <span class="attrib-value">${temper}</span></li>
+ </ul>
+</div>
+ </div>`
+  }
+}
--- utils.js
+++ utils.js
@@ -1,12 +1,5 @@
-export {diceRoll, render, splitDeck, initiateBattle}
-import { TrumpCard } from "./card"
-import { characters } from "./data"
+export {diceRoll}
 
-let player1Deck = []
-let player2Deck = []
-let battleDeck = []
-
-const cardsInPlay = document.querySelector('.cards-in-play')
 
 //diceRoll returns a random number between 1 and 6 to simulate a dice roll
 
@@ -14,22 +7,4 @@ function diceRoll () {
     return Math.floor(Math.random() * 6 + 1)
   }
 
-  function render(){
-    let player1ActiveCard = new TrumpCard(battleDeck[0])
-    let player2ActiveCard = new TrumpCard(battleDeck[1])
-      
-    cardsInPlay.innerHTML = `${player1ActiveCard.getCardHtml()} ${player2ActiveCard.getCardHtml()}`
-  }
 
-  function splitDeck(){
-    characters.forEach((character)=>{
-      // character.ref%2 !== 0? player1Deck.push(character) : player2Deck.push(character);
-      character.id === 1? player2Deck.push(character) : player1Deck.push(character);
-  })
-   }
-
-   function initiateBattle (){
-    battleDeck.unshift(player1Deck[0])
-    battleDeck.unshift(player2Deck[0])
-    render()
-  }
